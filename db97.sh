c33c41d2e7b669f7930eb3c31585b21fcd7bbfca
Yousef Elsayed <yousef4511@outlook.com>
2023-12-09 15:32:48 +0200

    Make db97 cotain the full path to the DBs
    Script no longer cd to $HOME/.db97 instead the $db97 will contain the
    full path "$HOME/.db97".
    
    This will make it easier to call other script from the source code
    folder

--- db97.sh
+++ db97.sh
@@ -12,30 +12,25 @@ echo ' ░░░░░░░░░░   ░░░░░░░░░░░      �
 echo '                                              ';
 echo '                                              ';
 
-db97=".db97"
+export db97="$HOME/.db97"
 opts=("Create DB" "List DB" "Connect DB" "Drop DB" "Exit")
 shopt -s extglob
 export LC_COLLATE=C
 
 #Validate DB97 exists or not
-if [[ ! -d ~/$db97 ]]
+if [[ ! -d $db97 ]]
 then
-  if ! mkdir ~/$db97; then
-    echo "Couldn't Create ~/$db97";
-    exit 1;
-  fi
-  echo -e "~/$db97 was created\n";
+  mkdir $db97 || { echo "Couldn't Create $db97"; exit 1; }
+  echo -e "$db97 was created\n";
 fi
 
 #Actual Driving Code
-cd ~/$db97 || { echo "Couldn't access $HOME/$db97"; exit 1; }
 
 PS3="DB97 #? "
 select choice in "${opts[@]}"
 do
   case $choice in
   "${opts[0]}")
-  #echo "To Do"
   echo "To Do"
   ;;
 
