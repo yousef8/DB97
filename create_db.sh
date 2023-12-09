ec0c0414e72cee03f4451ee59553b475b618d766
Yousef Elsayed <yousef4511@outlook.com>
2023-12-09 22:42:22 +0200

    Fix quit and db name validation regex

--- create_db.sh
+++ create_db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-# Check script was called by the driving script "db97"
+# Only run this script through "db97"
 [[ -z "$db97" ]] && { echo "This script isn't intended to run standalon insead as a part of the db97 script"; exit 1; }
 
 create_db() {
   # Validate DB Name 
-  [[ ! "$1" =~ ^[a-zA-Z]+[a-zA-Z0-9_-]+$ ]] && { echo "Invalid DB name can only contain Alphanumeric characters, _ and -. Can't start with numbers"; return 1; }
+  [[ ! "$1" =~ ^[a-zA-Z]+[a-zA-Z0-9_-]*$ ]] && { echo "Invalid DB name can only contain Alphanumeric characters, _ and -. Can't start with numbers"; return 1; }
 
   # Check if DB already exist
   [[ -d "$db97/$1" ]] && { echo "A database with the name $1 already exist"; return 1; }
@@ -17,7 +17,7 @@ create_db() {
 }
 
 read -rp "New DB name (or 'q' to quit): " db_name
-while [[ ! "$db_name" =~ ^[qQuUiItT]+$ ]]
+while [[ ! "$db_name" =~ ^[qQ]+([uU][iI][tT])?$ ]]
 do
   db_name=${db_name// /-}
 
