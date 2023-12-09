a5bd8381c6d745dab535659bcb8afdd2a1d6aeac
Yousef Elsayed <yousef4511@outlook.com>
2023-12-09 02:27:31 +0200

    Set up main driving code
    Create banner
    Handle initializing the DB97 hidden home directory
    Set up select menu

---
+++ db97.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+echo '                                                   ';
+echo ' ██████████   ███████████      ████████  ██████████';
+echo '░░███░░░░███ ░░███░░░░░███    ███░░░░███░███░░░░███';
+echo ' ░███   ░░███ ░███    ░███   ░███   ░███░░░    ███';
+echo ' ░███    ░███ ░██████████    ░░█████████      ███';
+echo ' ░███    ░███ ░███░░░░░███    ░░░░░░░███     ███';
+echo ' ░███    ███  ░███    ░███    ███   ░███    ███';
+echo ' ██████████   ███████████    ░░████████    ███';
+echo ' ░░░░░░░░░░   ░░░░░░░░░░░      ░░░░░░░░    ░░░';
+echo '                                              ';
+echo '                                              ';
+
+db97=".db97"
+opts=("Create DB" "List DB" "Connect DB" "Drop DB" "Exit")
+shopt -s extglob
+export LC_COLLATE=C
+
+#Validate DB97 exists or not
+if [[ ! -d ~/$db97 ]]
+then
+  mkdir ~/$db97
+  [[ $? != 0 ]] && { echo "Couldn't Create ~/$db97"; exit 1; }
+  echo -e "~/$db97 was created\n";
+fi
+
+#Actual Driving Code
+cd ~/$db97
+
+PS3="DB97 #? "
+select choice in "${opts[@]}"
+do
+  case $choice in
+  "${opts[0]}")
+  #echo "To Do"
+  echo "To Do"
+  ;;
+
+  "${opts[1]}")
+  echo "To Do"
+  ;;
+
+  "${opts[2]}")
+  echo "To Do"
+  ;;
+
+  "${opts[3]}")
+  echo "To Do"
+  ;;
+
+  "${opts[4]}")
+  echo "GoodBye"
+  break
+  ;;
+
+  *)
+  echo "Wrong Choice"
+  ;;
+esac
+done
+
+exit 0
